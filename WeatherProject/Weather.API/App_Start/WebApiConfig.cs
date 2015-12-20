0a9ccc9ae4a9f64a696e7a69bcbc7daee036f6a3
OskarKlintrot <oskar.klintrot@gmail.com>
2015-12-20 11:48:07 +0100

    Using JSON instead of XML

--- WeatherProject/Weather.API/App_Start/WebApiConfig.cs
+++ WeatherProject/Weather.API/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Weather.API
@@ -10,6 +11,7 @@ namespace Weather.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html")); // For using JSON instead of XML
 
             // Web API routes
             config.MapHttpAttributeRoutes();
