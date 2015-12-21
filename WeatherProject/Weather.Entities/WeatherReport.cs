b5bc58b87affed7dd2a69438b8f05185416b72b6
OskarKlintrot <oskar.klintrot@gmail.com>
2015-12-21 22:49:40 +0100

    Added tiny entity

---
+++ WeatherProject/Weather.Entities/WeatherReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Entities
+{
+    public class WeatherReport
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Location { get; set; }
+    }
+}
